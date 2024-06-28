99a604f9bd952a6504bda65efdd2b050db648fa1
Gowtham-converse <gowthamsakthivel@conversedatasolutions.com>
2024-06-28 19:46:18 +0530

    OAuth2PasswordRequestForm

--- app/routers/users.py
+++ app/routers/users.py
@@ -17,16 +17,21 @@ router=APIRouter(
 )
 
 local_storage=[]
-# @router.patch("/login")
-# async def login_form(form_data:OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db),):
+
+# @router.post("/login/")
+# async def login_swagger_form(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 #     user = auth.check_exist_user(db, form_data.username, form_data.password)
 #     if not user:
 #         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
+    
 #     access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
 #     refresh_token_expires = timedelta(days=auth.REFRESH_TOKEN_EXPIRE_DAYS)
+    
 #     access_token = auth.create_access_token(data={"sub": str(user.id)}, expires_delta=access_token_expires)
-#     refresh_token = auth.create_refresh_token(data={"sub":str(user.id)}, expires_delta=refresh_token_expires)
-#     crud.update_refresh_token(db, refresh_token,user.id) #update the refresh_token to database
+#     refresh_token = auth.create_refresh_token(data={"sub": str(user.id)}, expires_delta=refresh_token_expires)
+    
+#     crud.update_refresh_token(db, refresh_token, user.id)  # Update the refresh token in the database
+    
 #     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
 @router.post("/login")
